19ac1b390b3abdb03629bc762cd80c2d38e41d9e
presidentnixon91 <drewanix@yahoo.com>
2021-06-07 21:33:15 +1000

    HTML and CSS

---
+++ script.js
@@ -0,0 +1,21 @@
+// MATHEMATICAL FUNCTIONS
+// Add
+function add(a,b) {
+    return a + b;
+}
+// Subtract
+function subtract(a,b) {
+    return a - b;
+}
+// Multiply
+function multiple(a,b) {
+    return a * b;
+}
+// Divide
+function divide(a,b) {
+    return a / b;
+}
+// Operate
+function operate(operator,a,b) {
+    operator(a,b);
+}
