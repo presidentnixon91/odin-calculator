6d1c932fd7b38e4429a9c93385893badb6d0c91e
presidentnixon91 <drewanix@yahoo.com>
2021-06-07 22:01:21 +1000

    Some functionality

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ function divide(a,b) {
 function operate(operator,a,b) {
     operator(a,b);
 }
+// Values for storing numbers
+let calcUsed = false;
+let currentValue;
+// Populate input field on button press
+function addNumber(a) {
+    let currentDisplay = document.querySelector('#calc-input-field');
+    if(calcUsed) {
+        currentValue = currentDisplay.innerHTML + a;
+        currentDisplay.innerHTML = currentValue;
+    } else {
+        currentDisplay.innerHTML = a;
+        calcUsed = true;
+    }
+}
+// Events for clicking button
+let calcNumberButtons = document.querySelectorAll('.calc-number a');
+calcNumberButtons.forEach((btn) =>  {
+    // Event listener for each button
+    btn.addEventListener('click', (e) => {
+        let temp = e.target.innerHTML;
+        addNumber(temp);
+    });
+})
