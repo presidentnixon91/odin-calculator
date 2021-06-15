640b79b911495c670408266b7d61ae7136230211
presidentnixon91 <drewanix@yahoo.com>
2021-06-15 20:51:08 +1000

    Updates

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 // MATHEMATICAL FUNCTIONS
 // Add
-function add(a,b) {
-    return a + b;
+function add(c,d) {
+    return c + d;
 }
 // Subtract
-function subtract(a,b) {
-    return a - b;
+function subtract(c,d) {
+    return c - d;
 }
 // Multiply
-function multiple(a,b) {
-    return a * b;
+function multiple(c,d) {
+    return c * d;
 }
 // Divide
-function divide(a,b) {
-    return a / b;
+function divide(c,d) {
+    console.log(c / d);
 }
-// Operate
+// Operate using value taken from ID of operator button
 function operate(operator,a,b) {
+    console.log(operator);
     operator(a,b);
 }
 // Values for storing numbers
 let calcUsed = false;
+let oldValue;
 let currentValue;
+let operatorChoice;
+
+let currentDisplay = document.querySelector('#calc-input-field');
+
 // Populate input field on button press
 function addNumber(a) {
-    let currentDisplay = document.querySelector('#calc-input-field');
     if(calcUsed) {
         currentValue = currentDisplay.innerHTML + a;
         currentDisplay.innerHTML = currentValue;
     } else {
         currentDisplay.innerHTML = a;
+        currentValue = a;
         calcUsed = true;
     }
 }
-// Events for clicking button
+// Events for clicking number button
 let calcNumberButtons = document.querySelectorAll('.calc-number a');
 calcNumberButtons.forEach((btn) =>  {
     // Event listener for each button
@@ -41,4 +47,35 @@ calcNumberButtons.forEach((btn) =>  {
         let temp = e.target.innerHTML;
         addNumber(temp);
     });
-})
+});
+// Operators events
+let operatorButtons = document.querySelectorAll('.calc-operator');
+operatorButtons.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', () => {
+        operatorChoice = operatorBtn.getAttribute('id');
+        if(calcUsed) {
+            // Store the number on display as old value
+            oldValue = currentValue;
+            // Wipe the current value
+            currentValue = 0;
+            currentDisplay.innerHTML = currentValue;
+            calcUsed = false;
+        } else {
+            oldValue = 0;
+        }
+        console.log(calcUsed);
+        console.log(operatorChoice);
+        console.log(oldValue);
+        console.log(currentValue);
+    })
+});
+// Calculate the two numbers using operator
+let equalsButton = document.querySelector('#equals a');
+equalsButton.addEventListener('click', function() {
+    console.log(operatorChoice);
+    console.log(oldValue);
+    console.log(currentValue);
+    let x = Number(oldValue);
+    let y = Number(currentValue);
+    operate(operatorChoice,x,y);
+});
